59cb991cfd8351431843ac614dc14f61cacbbf75
kenyaachon <swahili1963@gmail.com>
2016-10-06 14:18:53 -0700

    Create Calculator1.java

---
+++ Calculator1.java
@@ -0,0 +1,12 @@
+
+public class calculater_main extends calculater_ui{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		calculater_ui x = new calculater_ui();
+		x.ui();
+		
+	}
+
+}
